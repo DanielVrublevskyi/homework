3dde3d081578dbe783460b8b3f377be943beddb3
Daniel <70908919+DanielVrublevskyi@users.noreply.github.com>
2024-11-20 13:54:45 +0200

    Bonus exercises

--- src/Lection4Homework.java
+++ src/Lection4Homework.java
@@ -1,12 +1,19 @@
 public class Lection4Homework {
     public static void main(String[] args) {
 
-        square(4);
+//        square(4);
+//
+//        cylinderVolume(2, 3);
+//
+//        exponentiation(2, 3);
 
-        cylinderVolume(2, 3);
-
-        exponentiation(2, 3);
-
+//        System.out.println(calculateSum(1, 1, 2, 5, 10, 1));
+        System.out.println(calculator("plus", 5,10, 15));
+        System.out.println(calculator("minus", 50,20, 10));
+        System.out.println(calculator("multiply", 2,3, 4));
+        System.out.println(calculator("divide", 100,5, 2));
+        System.out.println(calculator("divide", 100,0, 5));
+        System.out.println(calculator("dividde", 100,0, 5));
     }
 
 
@@ -30,4 +37,37 @@ public class Lection4Homework {
         }
         System.out.println(a + " to the " + b + " power = " + res);
     }
+
+    public static int calculateSum(int... elements) {
+
+        int res = 0;
+        for (int i = 0; i < elements.length; i++)
+        {
+           res = res + elements[i];
+        }
+        return res;
+    }
+
+    public static int calculator(String operation, int ... elements) {
+        int res = elements[0];
+        for (int i = 1; i < elements.length; i ++)
+        {
+            switch (operation){
+                case "plus": res = res + elements[i]; break;
+                case "minus":
+                    res = res - elements[i]; break;
+                case "multiply": res = res * elements[i]; break;
+                case "divide":
+                    if (res == 0 || elements[i] == 0){
+                        return 0;
+                    }
+                    res = res / elements[i]; break;
+                default:
+                    return -1;
+            }
+        }
+
+        return res;
+    }
 }
+
