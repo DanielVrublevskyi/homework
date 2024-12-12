fba8d60652f2f88810873108f13471de11131f45
Daniel <70908919+DanielVrublevskyi@users.noreply.github.com>
2024-12-12 19:47:49 +0200

    Lection 9 Homework

---
+++ src/main/java/aqaOopTask/Lection9_Homework.java
@@ -0,0 +1,19 @@
+package aqaOopTask;
+
+public class Lection9_Homework {
+    public static void main(String[] args) {
+
+        Person John = new Person("John", 30, "Engineer");
+        Person Mary = new Person("Mary", 25, "Teacher");
+        Person Bob = new Person("Bob", 35, "Doctor");
+
+        System.out.println("Name: " + John.getName() + ", Age: " + John.getAge() + ", Profession: " + John.getProfession());
+        System.out.println("Name: " + Mary.getName() + ", Age: " + Mary.getAge() + ", Profession: " + Mary.getProfession());
+        System.out.println("Name: " + Bob.getName() + ", Age: " + Bob.getAge() + ", Profession: " + Bob.getProfession());
+
+        Person Alice = new Person("Alice", 28, "Architect");
+        System.out.println("Name: " + Alice.getName() + ", Age: " + Alice.getAge() + ", Profession: " + Alice.getProfession());
+        Alice.setProfession("Designer");
+        System.out.println("Name: " + Alice.getName() + ", Age: " + Alice.getAge() + ", Profession: " + Alice.getProfession());
+    }
+}
