7429bdd0bb2c04e0f191f4fe6aa598b44bacd314
Daniel <70908919+DanielVrublevskyi@users.noreply.github.com>
2025-01-25 20:29:46 +0200

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,15 +1,9 @@
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static org.testng.Assert.fail;
 
 public class CalculatorTest{
     Calculator calculator = new Calculator();
-
-    @BeforeClass
-    public void beforeClass(){
-        
-    }
 
     @AfterClass
     public void afterClass(){
@@ -20,7 +14,7 @@ public class CalculatorTest{
     public void testAddition(){
 
         double actualRes = calculator.add(2, 3);
-        double expectedRes = 2+3;
+        double expectedRes = 5;
         if (actualRes == expectedRes){
             System.out.println("testAddition passed. \nResult = " + actualRes);
         }else {
@@ -31,7 +25,7 @@ public class CalculatorTest{
     @Test(description = "Subtraction", priority = 3)
     public void testSubtraction(){
         double actualRes = calculator.subtract(2, 3);
-        double expectedRes = 2-3;
+        double expectedRes = -1;
         if (actualRes == expectedRes){
             System.out.println("testSubtraction passed. \nResult = " + actualRes);
         }else {
@@ -42,7 +36,7 @@ public class CalculatorTest{
     @Test(description = "Multiplication", priority = 2)
     public void testMultiplication(){
         double actualRes = calculator.multiply(2, 3);
-        double expectedRes = 2*3;
+        double expectedRes = 6;
         if (actualRes == expectedRes){
             System.out.println("testMultiplication passed. \nResult = " + actualRes);
         }else {
@@ -52,19 +46,13 @@ public class CalculatorTest{
 
     @Test(description = "Division", priority = 1, expectedExceptions = {ArithmeticException.class})
     public void testDivisionByZero(){
-        double actualRes = calculator.divide(6, 0);
-        double expectedRes =  (double) 6 /0;
-        if (actualRes == expectedRes){
-            System.out.println("testDivision passed. \nResult = " + actualRes);
-        }else {
-            fail("testDivision failed. \nExpected result: " + expectedRes + "\nActual result = " + actualRes);
-        }
+        calculator.divide(6, 0);
     }
 
     @Test(description = "Division", priority = 0)
     public void testDivision(){
         double actualRes = calculator.divide(6, 2);
-        double expectedRes =  (double) 6 /2;
+        double expectedRes =  3;
         if (actualRes == expectedRes){
             System.out.println("testDivision passed. \nResult = " + actualRes);
         }else {
